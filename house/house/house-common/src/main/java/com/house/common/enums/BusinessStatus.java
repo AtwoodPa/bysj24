4f59ae4e8d6d2392cb5117223f979872495c4b44
supanpan <supanpan199919@163.com>
2024-03-05 13:11:36 +0800

    NewFeature
    house

---
+++ house/house/house-common/src/main/java/com/house/common/enums/BusinessStatus.java
@@ -0,0 +1,18 @@
+package com.house.common.enums;
+
+/**
+ * 操作状态
+ *
+ * @author house
+ */
+public enum BusinessStatus {
+    /**
+     * 成功
+     */
+    SUCCESS,
+
+    /**
+     * 失败
+     */
+    FAIL,
+}
---
+++ house/house/house-common/src/main/java/com/house/common/exception/DemoModeException.java
@@ -0,0 +1,13 @@
+package com.house.common.exception;
+
+/**
+ * 演示模式异常
+ *
+ * @author house
+ */
+public class DemoModeException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public DemoModeException() {
+    }
+}
